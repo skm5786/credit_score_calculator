a8d2649927c862b3e4a5f6864b9f7802380e9eec
skm5786 <sumitthakral3@gmail.com>
2022-07-21 11:39:16 +0530

    Initial Commit

---
+++ credit score calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  credit score calculator
+//
+//  Created by sumit thakral on 21/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
